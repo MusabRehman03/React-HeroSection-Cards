33f9eca92dd20bc9775fff504469eb839c76eed0
Musab Rehman <rehmanmusab0302@gmail.com>
2025-02-28 11:36:33 +0500

    fix: change the multiple state hooks to a single hook for form data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,38 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-    const [name,setName]=useState('Musab')
-    const [age,setAge]=useState('20')
-    function handleName(e) {
-        setName(e.target.value)
+    const [data,setData]=useState({
+        name:"",
+        age:""
+    })
+    function handleData(e){
+        const name=e.target.name
+        const value=e.target.value
+        return (setData((prevData)=>({
+            ...prevData,
+            [name]:value
+        })))
+        
     }
-    function handleAge(e){
-        setAge(e.target.value)
-    }
+    // function handleName(e) {
+    //     setName(e.target.value)
+    // }
+    // function handleAge(e){
+    //     setAge(e.target.value)
+    // }
     function handleSubmit(){
-        console.log("name: ",name);
-        console.log("age: ",age);
-
+        
+        console.log(data.name)
+        console.log(data.age)
+        if(data.age>90){
+            alert("invalid age")
+        }
         
     }
   return (
     <div className=' h-screen bg-black space-x-10'>
-      <input value={name} onChange={handleName} className= 'border border-b border-b-black rounded-xl px-3 py-1 focus:outline-none' type="text" />
-      <input value={age} onChange={handleAge} className= 'border border-b border-b-black rounded-xl px-3 py-1 focus:outline-none' type="number" />
+      <input name='name' value={data.name} onChange={handleData} className= 'border border-b border-b-black rounded-xl px-3 py-1 focus:outline-none' type="text" />
+      <input name='age' value={data.age} onChange={handleData} className= 'border border-b border-b-black rounded-xl px-3 py-1 focus:outline-none' type="number" />
       <button className='bg-green-500 text-black px-3 py-1 rounded-xl'  onClick={handleSubmit}>Submit</button>
     </div>
   )
